ecffa6a64d243b23beacb3b1f43bab9443197b36
Aysenur Kale <aysenur.kale@turkai.com>
2022-05-26 11:24:25 +0300

    first commit

---
+++ src/main/java/com/example/todoapp/TodoAppApplication.java
@@ -0,0 +1,44 @@
+package com.example.todoapp;
+
+import com.example.todoapp.entity.Todo;
+import com.example.todoapp.entity.User;
+import com.example.todoapp.repository.TodoRepository;
+import com.example.todoapp.repository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAppApplication implements CommandLineRunner {
+	@Autowired
+	private UserRepository userRepository;
+	@Autowired
+	private TodoRepository todoRepository;
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoAppApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		User user= new User();
+		user.setId(1L);
+		user.setUsername("ayse");
+		user.setPassword("test");
+
+		Todo todo = new Todo();
+		todo.setId(1L);
+		todo.setContent("icerik");
+
+		user.getTodoList().add(todo);
+
+		todoRepository.save(todo);
+		userRepository.save(user);
+
+
+
+
+	}
+}
---
+++ src/main/java/com/example/todoapp/entity/User.java
@@ -0,0 +1,64 @@
+package com.example.todoapp.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class User {
+
+	@Id
+	private Long id;
+
+	private String username;
+
+	private String password;
+
+	@OneToMany
+	private List<Todo> todoList= new ArrayList<>();
+
+	public User() {
+	}
+
+	public User(Long id, String username, String password, List<Todo> todoList) {
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.todoList = todoList;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public List<Todo> getTodoList() {
+		return todoList;
+	}
+
+	public void setTodoList(List<Todo> todoList) {
+		this.todoList = todoList;
+	}
+}
